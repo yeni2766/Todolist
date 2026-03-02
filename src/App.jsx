6387975c3b0cf7ac6650d6421bbca8e1293c2bfa
Yeni <yeni_o@Yenis-MacBook-Air.local>
2026-03-02 22:24:58 +0000

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './Todo'
-import Window from './Window'
 
 function App() {
   return(
